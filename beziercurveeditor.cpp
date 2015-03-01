0bedb9f8cf4e251a7995dfd38ee75bfe843f3553
Sandeep Datta <datta.sandeep@gmail.com>
2015-03-01 18:49:38 +0530

    Working.

--- beziercurveeditor.cpp
+++ beziercurveeditor.cpp
@@ -1,18 +1,24 @@
 #include "beziercurveeditor.h"
 
 #include <QPainter>
+#include <QMouseEvent>
 
 BezierCurveEditor::BezierCurveEditor(QWidget *parent)
     : QWidget(parent)
-    , m_blackPen(Qt::black)
-    , m_redPen(Qt::red)
-    , m_yellowPen(Qt::yellow)
-    , m_blackBrush(Qt::black)
-    , m_redBrush(Qt::red)
-    , m_yellowBrush(Qt::yellow)
+    , m_curvePen(Qt::black)
+    , m_dragging(false)
 {
-    m_blackPen.setWidth(2);
-    m_redPen.setWidth(2);
+    m_curvePen.setWidth(2);
+
+    m_colors[0] = Qt::yellow;
+    m_colors[1] = Qt::red;
+    m_colors[2] = Qt::blue;
+    m_colors[3] = Qt::yellow;
+
+    for (int i = 0; i < NUM_POINTS; i++) {
+        m_pens[i]    = QPen(m_colors[i]);
+        m_brushes[i] = QBrush(m_colors[i]);
+    }
 }
 
 BezierCurveEditor::~BezierCurveEditor()
@@ -20,13 +26,36 @@ BezierCurveEditor::~BezierCurveEditor()
 
 }
 
+void BezierCurveEditor::mousePressEvent(QMouseEvent *event)
+{
+    for(int i = 0; i < NUM_POINTS; i++) {
+        if(distance(m_points[i], event->pos()) <= POINT_RADIUS) {
+            m_selectedPoint = i;
+            m_dragging = true;
+            break;
+        }
+    }
+}
+
+void BezierCurveEditor::mouseMoveEvent(QMouseEvent *event)
+{
+    if(m_dragging) {
+        m_points[m_selectedPoint] = event->pos();
+    }
+    update();
+}
+
+void BezierCurveEditor::mouseReleaseEvent(QMouseEvent *)
+{
+    m_dragging = false;
+}
+
 void BezierCurveEditor::resizeEvent(QResizeEvent *)
 {
-    m_startPoint = QPointF(20, height() - 20);
-    m_endPoint   = QPointF(width() - 20, 20);
-
-    m_controlPoint2 = QPointF(20, 20);
-    m_controlPoint1 = QPointF(width() - 20, height() - 20);
+    m_points[StartPoint]    = QPointF(20, height() - 20);
+    m_points[ControlPoint1] = QPointF(width() - 20, height() - 20);
+    m_points[ControlPoint2] = QPointF(20, 20);
+    m_points[EndPoint]      = QPointF(width() - 20, 20);
 }
 
 void BezierCurveEditor::paintEvent(QPaintEvent *)
@@ -35,21 +64,16 @@ void BezierCurveEditor::paintEvent(QPaintEvent *)
 
     painter.setRenderHint(QPainter::Antialiasing, true);
 
-    painter.setPen(m_blackPen);
+    painter.setPen(m_curvePen);
     QPainterPath path;
-    path.moveTo(m_startPoint);
-    path.cubicTo(m_controlPoint1, m_controlPoint2, m_endPoint);
+    path.moveTo(m_points[StartPoint]);
+    path.cubicTo(m_points[ControlPoint1], m_points[ControlPoint2], m_points[EndPoint]);
     painter.drawPath(path);
 
-    painter.setPen(m_yellowPen);
-    painter.setBrush(m_yellowBrush);
-    painter.drawEllipse(m_startPoint, 6, 6);
-    painter.drawEllipse(m_endPoint, 6, 6);
-
-    painter.setPen(m_redPen);
-    painter.setBrush(m_redBrush);
-    painter.drawEllipse(m_controlPoint1, 6, 6);
-    painter.drawEllipse(m_controlPoint2, 6, 6);
-
+    for (int i = 0; i < NUM_POINTS; i++) {
+        painter.setPen(m_pens[i]);
+        painter.setBrush(m_brushes[i]);
+        painter.drawEllipse(m_points[i], POINT_RADIUS, POINT_RADIUS);
+    }
 }
 
--- beziercurveeditor.h
+++ beziercurveeditor.h
@@ -15,24 +15,42 @@ public:
 protected:
     void paintEvent(QPaintEvent *);
     void resizeEvent(QResizeEvent *);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *);
+
 signals:
 
 public slots:
 
 private:
-    QPointF     m_controlPoint1;
-    QPointF     m_controlPoint2;
-    QPointF     m_startPoint;
-    QPointF     m_endPoint;
+    enum PointIndices {
+        StartPoint = 0,
+        ControlPoint1 = 1,
+        ControlPoint2 = 2,
+        EndPoint = 3
+    };
 
-    QPen        m_blackPen;
-    QPen        m_redPen;
-    QPen        m_yellowPen;
+   //Functions
+private:
+    qreal distance(QPointF a, QPointF b) {
+        QPointF diff = a - b;
+        return sqrt(diff.x()*diff.x() + diff.y()*diff.y());
+    }
 
-    QBrush      m_blackBrush;
-    QBrush      m_redBrush;
-    QBrush      m_yellowBrush;
+private:
+    const int NUM_POINTS = 4;
+    const qreal POINT_RADIUS = 4.0;
 
+    QPointF         m_points[4];
+    QPen            m_pens[4];
+    QBrush          m_brushes[4];
+    Qt::GlobalColor m_colors[4];
+
+    QPen        m_curvePen;
+
+    bool        m_dragging;
+    int         m_selectedPoint;
 };
 
 #endif // BEZIERCURVEEDITOR_H
